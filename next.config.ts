9599d6ab51252f75c424120b704ceb788dc85c05
Simon Luke <simon.luke@impactoryinstitute.com>
2025-11-22 16:42:49 +0000

    fix Next.js 15.3.3 (outdated) Webpack Console Error
    Error: Invalid or

--- next.config.ts
+++ next.config.ts
@@ -1,79 +1,9 @@
 
 import type {NextConfig} from 'next';
-import withPWAInit from 'next-pwa';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: !isProduction,
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts',
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/images\.unsplash\.com\//,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'unsplash-images',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/lh3\.googleusercontent\.com\//,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-user-images',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /^https?.* /,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
-});
-
-
 const nextConfig: NextConfig = {
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1763798265683.cluster-y3k7ko3fang56qzieg3trwgyfg.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       {
@@ -110,5 +40,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
-
+export default nextConfig;
