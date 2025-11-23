fd88a420ee00690e31042989f942669030be1791
Simon Luke <simon.luke@impactoryinstitute.com>
2025-11-23 11:37:30 +0000

    fix these Server Error Error: Cannot find module './682.js' Require stac

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,23 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {isDev, isServer} from 'genkit/internal/util';
+
+if (isDev() && isServer()) {
+  const {NodeSDK} = require('@opentelemetry/sdk-node');
+  const {
+    OTLPTraceExporter,
+  } = require('@opentelemetry/exporter-trace-otlp-http');
+  const {
+    getNodeAutoInstrumentations,
+  } = require('@opentelemetry/auto-instrumentations-node');
+
+  const sdk = new NodeSDK({
+    traceExporter: new OTLPTraceExporter(),
+    instrumentations: [getNodeAutoInstrumentations()],
+  });
+
+  sdk.start();
+}
 
 export const ai = genkit({
   plugins: [googleAI()],
