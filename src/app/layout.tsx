87820fa25696bd91f6a0d97ddcfdf091181f91c9
Simon Luke <simon.luke@impactoryinstitute.com>
2025-11-23 11:08:33 +0000

    make sure the PWA uses the apprioriate favicon and displays it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="theme-color" content="#3F51B5" />
       </head>
       <body className={`${inter.variable} ${spaceGrotesk.variable} font-body antialiased`}>
